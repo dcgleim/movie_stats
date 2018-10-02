e8ce1cf77744007401c94dce50d646cac15ef7b8
dcgleim <derek.gleim@unizin.org>
2018-10-02 15:32:53 -0500

    a few little things
    - it's 2018.  No one uses backticks anymore.
    - moved command into docker file.
    - made run.sh executable, and matched the shebang to what I think
      the intention was (based on the docs).

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
-#! /bin/bash
+#! /bin/sh
 set -e
 
 docker build -t movie_stats ./docker
-docker run --memory=4g -p 8889:8888 -p 4040:4040 -i --rm -v `pwd`/notebooks:/notebooks -t movie_stats bash -c 'pyspark --driver-memory 16g'
+
+docker run --rm \
+  --memory=4g \
+  -p 8889:8888 -p 4040:4040 \
+  -v "$(pwd)/notebooks:/notebooks" \
+  movie_stats
