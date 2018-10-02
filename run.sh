675b31ac14951946632a931ac28292a530376a5d
George Gamble Ruff <gruff@Georges-MBP.attlocal.net>
2018-10-02 14:44:03 -0500

    something

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 set -e
 
-docker build -t spark_workshop ./docker
-docker run --memory=4g -p 8889:8888 -p 4040:4040 -i --rm -v `pwd`/notebooks:/notebooks -t spark_workshop bash -c 'pyspark --driver-memory 16g'
+docker build -t movie_stats ./docker
+docker run --memory=4g -p 8889:8888 -p 4040:4040 -i --rm -v `pwd`/notebooks:/notebooks -t movie_stats bash -c 'pyspark --driver-memory 16g'
