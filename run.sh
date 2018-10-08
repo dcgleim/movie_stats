3c3782f4ff8f83ad3f069630c6ed3e58fd676bd9
George Gamble Ruff <gruff@Georges-MBP.attlocal.net>
2018-10-07 23:22:58 -0500

    All the things

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 set -e
 
 docker build -t movie_stats ./docker
-docker run --memory=4g -p 8889:8888 -p 4040:4040 -i --rm -v `pwd`/notebooks:/notebooks -t movie_stats bash -c 'pyspark --driver-memory 16g'
+docker run --memory=4g -p 8889:8888 -i --rm -v `pwd`/notebooks:/notebooks -t movie_stats bash -c 'jupyter notebook --port 8888 --ip=0.0.0.0 --allow-root --config /notebooks/.config.py --no-browser --notebook-dir=/notebooks'
